1241768b272c47bfd0658c261dbbb91d3702a1a4
stephantoine <stephouille1022@gmail.com>
2026-02-05 19:34:30 +0100

    ajout responsif header et footer + fin page d'accueil

--- laravel/lite-app/resources/js/components/musecomponents/Footer.tsx
+++ laravel/lite-app/resources/js/components/musecomponents/Footer.tsx
@@ -1,48 +1,56 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <nav className='grid grid-cols-5'>
-        <img src="/logo.svg" alt="logo" />
-
-        {/* legal */}
-        <div className='flex flex-col gap-2'>
-            <a href="">Légal</a>
-            <div className='flex flex-col'>
-                <a href="">Mentions légales</a>
-                <a href="">Consentement</a>
+    return (
+        <nav className='flex gap-8 p-4 flex-col sm:flex-row sm:gap-14'>
+            {/* Logo */}
+            <div className='flex justify-center'>
+                <img src="/logo.svg" alt="logo" className="w-auto h-8" />
             </div>
-        </div>
 
-        {/* docs */}
-        <div className='flex flex-col gap-2'>
-            <a href="">Documentation</a>
-            <div className='flex flex-col'>
-                <a href="">Installation</a>
-                <a href="">API</a>
-                <a href="">Utilisation</a>
-                <a href="https://laravel.com/docs">Laravel</a>
-            </div>
-        </div>
+            <div className='grid grid-cols-1 gap-6
+                            sm:grid-cols-2 sm:gap-8
+                            md:grid-cols-3 md:gap-10
+                            lg:grid-cols-4 lg:gap-12'>
+                {/* legal */}
+                <div className='flex flex-col gap-2 text-center sm:text-left'>
+                    <a href="" className="font-semibold hover:underline">Légal</a>
+                    <div className='flex flex-col text-sm'>
+                        <a href="" className="hover:underline">Mentions légales</a>
+                        <a href="" className="hover:underline">Consentement</a>
+                    </div>
+                </div>
 
-        {/* liens */}
-        <div className='flex flex-col gap-2'>
-            <p>Liens externes</p>
-            <div className='flex flex-col'>
-                <a href="">IUT de Lannion</a>
-            </div>
-        </div>
+                {/* docs */}
+                <div className='flex flex-col gap-2 text-center sm:text-left'>
+                    <a href="" className="font-semibold hover:underline">Documentation</a>
+                    <div className='flex flex-col text-sm'>
+                        <a href="" className="hover:underline">Installation</a>
+                        <a href="" className="hover:underline">API</a>
+                        <a href="" className="hover:underline">Utilisation</a>
+                        <a href="https://laravel.com/docs" target="_blank" rel="noopener noreferrer" className="hover:underline">Laravel</a>
+                    </div>
+                </div>
 
-        {/* contact */}
-        <div className='flex flex-col gap-2'>
-            <p>Contact</p>
-            <div className='flex flex-col'>
-                <p>7 Rue Édouard Branly 22300 Lannion</p>
-                <a href="">Contacter le support</a>
+                {/* liens */}
+                <div className='flex flex-col gap-2 text-center sm:text-left'>
+                    <p className="font-semibold">Liens externes</p>
+                    <div className='flex flex-col text-sm'>
+                        <a href="" className="hover:underline">IUT de Lannion</a>
+                    </div>
+                </div>
+
+                {/* contact */}
+                <div className='flex flex-col gap-2 text-center sm:text-left'>
+                    <p className="font-semibold">Contact</p>
+                    <div className='flex flex-col text-sm'>
+                        <p>7 Rue Édouard Branly 22300 Lannion</p>
+                        <a href="" className="hover:underline">Contacter le support</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Footer
--- laravel/lite-app/resources/js/components/musecomponents/Navbar.tsx
+++ laravel/lite-app/resources/js/components/musecomponents/Navbar.tsx
@@ -9,40 +9,40 @@ type NavbarProps = {
 
 const Navbar = ({ user }: NavbarProps) => {
   return (
-    <nav className="flex items-center justify-between">
+    <nav className="flex flex-wrap items-center justify-between gap-4 p-4">
       {/* Left */}
-      <div className="flex items-center gap-6">
-        <img src="/logo.svg" alt="logo"/>
+      <div className="flex flex-wrap items-center gap-4">
+        <img src="/logo.svg" alt="logo" />
         {user && (
           <>
-            <div>
+            <div className="hidden sm:block">
               <p>search</p>
             </div>
-            <div className="flex gap-4">
-              <a href="/" title="Accueil">Accueil</a>
-              <a href="/favoris" title="Favoris">Favoris</a>
-              <a href="/decouverte" title="Découverte">Découvrir</a>
+            <div className="flex flex-wrap gap-4 text-sm">
+              <a href="/" title="Accueil" className="hover:underline">Accueil</a>
+              <a href="/favoris" title="Favoris" className="hover:underline">Favoris</a>
+              <a href="/decouverte" title="Découverte" className="hover:underline">Découvrir</a>
             </div>
           </>
         )}
       </div>
 
       {/* Right */}
-      <div>
+      <div className="flex flex-wrap items-center gap-4 text-sm">
         {user ? (
-          <div className="flex items-center gap-3">
-            <a href="/profile">Mon profil</a>
+          <>
+            <a href="/profile" className="hover:underline">Mon profil</a>
             <img src={user.avatar} alt="Photo de profil" width={32} height={32} className="rounded-full" />
-          </div>
+          </>
         ) : (
-          <div className="flex items-center gap-3">
-            <a href="/register">Inscription</a>
+          <>
+            <a href="/register" className="hover:underline">Inscription</a>
             <Button>
               <Link href="/login" className='text-white'>
                 Connexion
               </Link>
             </Button>
-          </div>
+          </>
         )}
       </div>
     </nav>
