7a2b72e45fc8757a9586c005ae7e316e4511225e
stephantoine <stephouille1022@gmail.com>
2026-02-06 16:12:29 +0100

    fix navbar and footer when connected

--- laravel/lite-app/resources/js/layouts/app/app-sidebar-layout.tsx
+++ laravel/lite-app/resources/js/layouts/app/app-sidebar-layout.tsx
@@ -2,6 +2,9 @@ import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { usePage } from '@inertiajs/react';
+import type { SharedData } from '@/types';
+
 import type { AppLayoutProps } from '@/types';
 import Navbar from '@/components/musecomponents/Navbar';
 import Footer from '@/components/musecomponents/Footer';
@@ -10,6 +13,9 @@ export default function AppSidebarLayout({
     children,
     breadcrumbs = [],
 }: AppLayoutProps) {
+
+    const { auth } = usePage<SharedData>().props;
+    
     return (
         <AppShell variant="sidebar">
             <AppSidebar />
@@ -19,7 +25,7 @@ export default function AppSidebarLayout({
 
                 <header className="w-full shadow-[0_4px_12px_rgba(0,0,0,0.08)]">
                     <div className="mx-auto w-full max-w-[675px] lg:max-w-4xl text-sm">
-                        <Navbar />
+                        <Navbar user={auth.user}/>
                     </div>
                 </header>
 
@@ -27,11 +33,13 @@ export default function AppSidebarLayout({
                     {children}
                 </main>
                 
-                <footer className="w-full shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
-                    <div className="mx-auto w-full max-w-[675px] lg:max-w-4xl py-6 text-sm">
-                        <Footer />
-                    </div>
-                </footer>
+                {!auth.user && (
+                    <footer className="w-full shadow-[0_-4px_12px_rgba(0,0,0,0.08)]">
+                        <div className="mx-auto w-full max-w-[675px] lg:max-w-4xl py-6 text-sm">
+                            <Footer />
+                        </div>
+                    </footer>
+                )}
                 
             </AppContent>
         </AppShell>
