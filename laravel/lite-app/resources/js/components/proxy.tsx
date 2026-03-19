550b0b9ec54bb11af0c5de11816c0443dd7c46fc
stephantoine <stephouille1022@gmail.com>
2026-03-19 09:51:01 +0100

    fix images

--- laravel/lite-app/resources/js/components/proxy.tsx
+++ laravel/lite-app/resources/js/components/proxy.tsx
@@ -1,10 +1,14 @@
 // Proxy les URLs externes pour éviter les problèmes CORS
 export function proxyUrl(url: string | null | undefined): string | undefined {
     if (!url) return undefined;
-    // Si c'est déjà une URL locale, ne pas proxifier
-    if (url.startsWith('/') || url.startsWith(window.location.origin)) {
+
+    //si c'est un lien vers le freemusicarchive, on le proxifie
+    if (url.includes('freemusicarchive.org')) {
+        return `/proxy?url=${encodeURIComponent(url)}`;
+    }
+
+    // Proxifier les URLs externes
+    else {
         return url;
     }
-    // Proxifier les URLs externes
-    return `/proxy?url=${encodeURIComponent(url)}`;
 }
