83cf57cb98483077216588f25f214283d43acaa7
Scover <>
2026-03-19 10:58:34 +0100

    better scripts + use transactions instead of refreshdatabase refreshdatabase relies on migration to reconstructs the db but we seed it internally

--- laravel/lite-app/tests/TestCase.php
+++ laravel/lite-app/tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        DB::beginTransaction();
+
+        $this->beforeApplicationDestroyed(function (): void {
+            while (DB::transactionLevel() > 0) {
+                DB::rollBack();
+            }
+        });
+    }
 }
